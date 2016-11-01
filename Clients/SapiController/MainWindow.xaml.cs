ddd6c4d39f032d4d6ebcbbdf48abea3e06bd2347
Christian Sack SP4 <christian@sack.at>
2016-11-01 12:07:19 +0100

    initial thoughts

--- Clients/SapiController/MainWindow.xaml.cs
+++ Clients/SapiController/MainWindow.xaml.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿// Copyright (c) 2016 All rights reserved Christian Sack
+// <author>Christian Sack</author>
+// <email>christian@sack.at</email>
+// <website>christian.sack.at</website>
+// <date>2016-11-01</date>
+
+using System;
+using CsWpfBase.Themes.Controls.Containers;
+
+
+
+
+
 
 namespace SapiController
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
+	/// <summary>Interaction logic for MainWindow.xaml</summary>
+	public partial class MainWindow : CsWindow
 	{
 		public MainWindow()
 		{
